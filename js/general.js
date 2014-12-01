66ab2d85995914cf37397807d033fb02cd0fae9c
alkalinegenome <alkalinegenome@yahoo.com>
2014-12-01 16:06:27 -0500

    Nav-Top Background amended
    + When scrolling, the bg would get left behind in nav movement so items
    could be seen barring a proper view of the nav links

--- js/general.js
+++ js/general.js
@@ -7,6 +7,7 @@ var scrollTop = $(window).scrollTop();
 if (scrollTop > stickyNavTop + 375) {   //375 works
     $('.nav').addClass('sticky');  
 	$('.nav img').css({ 'display': 'none' });	
+	$('.nav').css({'background-color':'#F5F5F5'});
 } else {  
 	$('.nav img').css({ 'display': 'block' });	
     $('.nav').removeClass('sticky');   
