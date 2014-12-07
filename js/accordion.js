d5651ce31dc8ef43e2c47ceaaae9fe9de29310cf
alkalinegenome <alkalinegenome@yahoo.com>
2014-12-07 02:22:30 -0500

    Hamburger Nav for Mobile Established
    + works only for mobile and small-tablet screens
    + hamburger nav is responsive
    - JS interferes,

--- js/accordion.js
+++ js/accordion.js
@@ -5,11 +5,3 @@ $(function() {
 		$(this).next('p').slideToggle('fast');
 	});
 });
-
-$(function() {
-	'use strict';
-	$('.navccordion').find('nav-top').on('click', '.menu-toggle', function() {
-		$(this).parent().siblings().find('li').slideUp('fast');
-		$(this).next('li').slideToggle('fast');
-	});
-});
--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,6 @@
 // JavaScript Document -- TOGGLE JS
-var navTop = document.querySelector('.nav-top');
+/*
+var navTop = document.querySelector('.nav-toggle');
 
 document.querySelector('.nav-btn').addEventListener('click', function(e){
 	e.preventDefault();
@@ -9,4 +10,62 @@ document.querySelector('.nav-btn').addEventListener('click', function(e){
 		navTop.setAttribute('data-state','expanded');
 
 	}
+}); */
+
+// Sticky Nav
+
+$(document).ready(function() {  
+var stickyNavTop = $('.nav-top').offset().top;  
+  
+var stickyNav = function(){  
+var scrollTop = $(window).scrollTop();  
+       
+if (scrollTop > stickyNavTop + 375) {   // + 375 works
+    $('.nav').addClass('sticky');  
+	$('.nav img').css({ 'transform': 'translateY(-300px)' });		
+	$('.nav img').css({ 'width': '0' });		
+	$('.nav').css({'background-color':'#F5F5F5'});
+	$('.nav-top').css({'border-top':'1px solid red'});
+
+	//			alert('active');
+} else {  
+	$('.nav img').css({ 'transform': 'translateY(0px)' });		
+	$('.nav img').css({ 'width': '15%' });		
+    $('.nav').removeClass('sticky');   
+}  
+};  
+  
+stickyNav();  
+
+$(window).scroll(function() {  
+    stickyNav();  
+});  
+}); 
+
+//nav icons 
+
+var $navicons = $('.menu-btn');
+var $navsection = $('.nav-top');
+
+$navsection.waypoint(function () {
+	$navicons.addClass('js-navicon-launch');
+}, {offset: '50%'});
+
+// Nav Toggle 2
+/*
+$(function() {
+	'use strict';
+	$('.accordion').find('li').on('click', 'h3', function() {
+		$(this).parent().siblings().find('p').slideUp('fast');
+		$(this).next('p').slideToggle('fast');
+	});
+}); */
+
+$(function() {
+	'use strict';
+	$('.toggle').find('a').on('click', 'h3', function() {
+		$(this).find('nav').slideUp('fast');
+		alert('active');
+/*		$(this).next('.nav-icons').slideToggle('fast');*/
+	});
 });
