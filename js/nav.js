fd3b141d1f3adee9d8e35dc963ed68aaec5143ac
alkalinegenome <alkalinegenome@yahoo.com>
2014-12-07 22:12:57 -0500

    Nav Squish *should* work on non-mobile screens?

--- js/nav.js
+++ js/nav.js
@@ -20,21 +20,24 @@ var stickyNavTop = $('.nav-top').offset().top;
 var stickyNav = function(){  
 var scrollTop = $(window).scrollTop();  
        
-if (scrollTop > stickyNavTop + 375) {   // + 375 works
-    $('.nav').addClass('sticky');  
-	$('.nav img').css({ 'transform': 'translateY(-300px)' });		
-	$('.nav img').css({ 'width': '0' });		
-	$('.nav').css({'background-color':'#F5F5F5'});
-	$('.nav-top').css({'border-top':'1px solid red'});
-
-	//			alert('active');
-} else {  
-	$('.nav img').css({ 'transform': 'translateY(0px)' });		
-	$('.nav img').css({ 'width': '15%' });		
-    $('.nav').removeClass('sticky');   
-}  
-};  
-  
+if (window.screen.width >= 768) {	
+	if (scrollTop > stickyNavTop + 375) {   // + 375 works
+		$('.nav').addClass('sticky');  
+		$('.nav img').css({ 'transform': 'translateY(-300px)' });		
+		$('.nav img').css({ 'width': '0' });		
+		$('.nav').css({'background-color':'#F5F5F5'});
+		$('.nav-top li').css({'border-bottom':'2px solid #FA8072'});
+	
+		//			alert('active');
+	} else {  
+		$('.nav img').css({ 'transform': 'translateY(0px)' });		
+		$('.nav img').css({ 'width': '15%' });		
+		$('.nav').removeClass('sticky');   
+		$('.nav-top li').css({'border-bottom':'none'});
+	}  
+	};  
+}
+ 
 stickyNav();  
 
 $(window).scroll(function() {  
